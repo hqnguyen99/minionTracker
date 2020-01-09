64943ff40c14c76f860f653c428e29404ee30802
hqnguyen99 <45479772+hqnguyen99@users.noreply.github.com>
2020-01-09 13:22:56 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args){
-
+         String welcomeLine= "Welcome to the Evil Minion Tracker (tm)\n" +
+                "by Your Name Here.  ";
+         String firstWelcomeLine= "Welcome to the Evil Minion Tracker (tm)";
+        // Show welcome line
+        printStarBorder(firstWelcomeLine);
+        System.out.println(welcomeLine);
+        printStarBorder(firstWelcomeLine);
+        System.out.println();
     }
 }
