07b847c15db41c78c459e32a6c5fd482939c50d1
hqnguyen99 <45479772+hqnguyen99@users.noreply.github.com>
2020-01-10 08:46:16 -0800

    Update Menu.java

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -2,56 +2,45 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Menu {
-    private MinionManager manager;
-    private Scanner in = new Scanner(System.in);
+    MinionManager manager= new MinionManager();
+    Scanner in = new Scanner(System.in);
     private String title= "Main Menu";
-
+    private String welcomeLine= "Welcome to the Evil Minion Tracker (tm)\n" +
+                                "by Your Name Here.  ";
+    private String firstWelcomeLine= "Welcome to the Evil Minion Tracker (tm)";
     private String[] menuOptions=  {"List minions" , "Add a new minion",
                                     "Remove minion" , "Attribute an evil deed to a minion",
                                     "Debug dump of minion details", "Exit"};
-    public void show() {
-
+    public void showWelcomeLine(){
+        // Show welcome line
+        printStarBorder(firstWelcomeLine);
+        System.out.println(welcomeLine);
+        printStarBorder(firstWelcomeLine);
+        System.out.println();
+    }
+    public void showMenu() {
         // Show menu
-        boolean isDone = false;
-        while (!isDone) {
-            printStarBorder("*" + title + "*");
-            System.out.println("*" + title + "*");
-            for (int i = 0; i < menuOptions.length; i++) {
-                int optionIndex = i + 1;
-                System.out.println(optionIndex + ". " + menuOptions[i]);
-            }
-
-            int choice = in.nextInt();
-
-            switch(choice) {
-                case (1):
-                    listMinion();
-
-                case(2):
-                    addMinion();
-
-                case (3):
-
-
-
-            }
+        printStarBorder("*" + title + "*");
+        System.out.println("*" + title + "*");
+        for (int i = 0; i < menuOptions.length; i++) {
+            int optionIndex = i + 1;
+            System.out.println(optionIndex + ". " + menuOptions[i]);
         }
     }
-    private void printStarBorder(String str){
+    public void printStarBorder(String str){
         char[] stringToChars= str.toCharArray();
         for (char letter: stringToChars){
             System.out.print("*");
         }
         System.out.println();
     }
-
-    private void listMinion(){
+    public void listMinion(){
         System.out.println("List of Minions:");
         printStarBorder("List of Minions:");
         manager.printMinionList();
     }
 
-    private void addMinion(){
+    public void addMinion(){
         System.out.println("Enter minion's name: ");
         String minionName = in.nextLine();
         System.out.println("Enter minion's height: ");
@@ -59,10 +48,24 @@ public class Menu {
         manager.add(new Minion(minionName,minionHeight ));
     }
 
-    private void removeMinion(){
+    public void removeMinion(){
         listMinion();
         System.out.println("Enter 0 to cancel");
-
+        int selectMinionToRemove = in.nextInt();
+        if (selectMinionToRemove != 0){
+            manager.remove(selectMinionToRemove);
+        }
+    }
+    public void attributeAnDevilDeed(){
+        listMinion();
+        System.out.println("Enter 0 to cancel");
+        int selectMinionToAttribute = in.nextInt();
+        if (selectMinionToAttribute != 0){
+            manager.attributeAnEvilDeed(selectMinionToAttribute);
+        }
+    }
+    public void debugDumpOfMinionDetails(){
+        manager.printDumpObject();
     }
 
 }
